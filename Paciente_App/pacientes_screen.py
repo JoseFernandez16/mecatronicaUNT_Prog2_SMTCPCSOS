d74f95f98279b3659bff4b701dc178bf27100f72
Carlos Rocha <ad7rocha@gmail.com>
2021-03-03 15:10:31 -0500

    Terminado

--- Paciente_App/pacientes_screen.py
+++ Paciente_App/pacientes_screen.py
@@ -8,9 +8,10 @@ from kivy.graphics import Color,RoundedRectangle
 from functools import partial
 from io import open
 import time
-
+import serial
 from conexion_BD import MedicionesPacientes
 from conexion_BD import Conexion_BD
+ser = serial.Serial("COM6", 115200)
 
 kv="""
 <PacientesScreen>:
@@ -40,30 +41,36 @@ kv="""
                 text_color: 1, 0, 0, 1
                 line_color: 0, 0, 1, 1
                 pos_hint:{'center_x':.2,'center_y':.5}
+                on_release:root.Temperatura()
             MDRectangleFlatButton:
                 text: "Medir porcentaje de saturacion de oxigeno"
                 theme_text_color: "Custom"
                 text_color: 1, 0, 0, 1
                 line_color: 0, 0, 1, 1
                 pos_hint:{'center_x':.5,'center_y':.5}
+                on_release:root.SPO2()
             MDRectangleFlatButton:
                 text: "Medir pulso cardiaco"
                 theme_text_color: "Custom"
                 text_color: 1, 0, 0, 1
                 line_color: 0, 0, 1, 1
                 pos_hint:{'center_x':.8,'center_y':.5}
+                on_release:root.heart()
 
         GridLayout:
             size_hint:(1,.2)
             cols:3
             MDLabel:
-                text:'temperatura'
+                id:TEMPERATURA
+                text:""
                 halign:'center'
             MDLabel:
-                text:'porcentaje de saturacion de oxigeno'
+                id:SPO2
+                text:''
                 halign:'center'
             MDLabel:
-                text:'pulso'
+                id:PULSO
+                text:''
                 halign:'center'
 
         FloatLayout:
@@ -104,7 +111,89 @@ class PacientesScreen(MDScreen):
         self.mediciones=MedicionesPacientes(dni,fecha,fecha_int,hora,hora_int,pulso,temperatura,oxigeno)
 
         self.insertar.insertar_dato(self.mediciones.toCollection())
-        
+    def Temperatura(self):
+  
+        ser.write(chr(49).encode('ascii'))
+        cc2 = ""
+        sal = 0
+        tempLista = []
+        while True:
+            cc = str(ser.readline())
+            # print(str(cc[2:][:-5]))
+
+            while cc[2:][:-5] == "49":
+                cc2 = str(ser.readline())
+                if cc2[2:][:-5] != "a":
+                    tempLista.append(float(cc2[2:][:-5]))
+                    ptemp = sum(tempLista)/len(tempLista)
+                    # print(tempLista)
+                    print(ptemp)
+
+                    # print(str(cc2[2:][:-5]))
+
+                if cc2[2:][:-5] == "a":
+                    sal = 1
+                    break
+            if sal == 1:
+                break
+        self.ids['TEMPERATURA'].text = str(round(ptemp, 2))
+
+    def SPO2(self):
+
+        ser.write(chr(50).encode('ascii'))
+        cc2 = ""
+        sal = 0
+        tempLista = []
+        while True:
+            cc = str(ser.readline())
+            # print(str(cc[2:][:-5]))
+
+            while cc[2:][:-5] == "50":
+                cc2 = str(ser.readline())
+                if cc2[2:][:-5] != "a" and cc2[2:][:-5] != "-999":
+                    tempLista.append(float(cc2[2:][:-5]))
+                    ptemp = sum(tempLista)/len(tempLista)
+                    # print(tempLista)
+                    print(ptemp)
+                    # print(str(cc2[2:][:-5]))
+                elif len(tempLista) == 0:
+                    ptemp = 0
+
+                if cc2[2:][:-5] == "a":
+                    sal = 1
+                    break
+            if sal == 1:
+                break
+
+        self.ids['SPO2'].text = str(round(ptemp, 2))
+
+    def heart(self):
+
+        ser.write(chr(51).encode('ascii'))
+        cc2 = ""
+        sal = 0
+        tempLista = []
+        while True:
+            cc = str(ser.readline())
+            # print(str(cc[2:][:-5]))
+
+            while cc[2:][:-5] == "51":
+                cc2 = str(ser.readline())
+                if cc2[2:][:-5] != "a" and cc2[2:][:-5] != "-999":
+                    tempLista.append(float(cc2[2:][:-5]))
+                    ptemp = sum(tempLista)/len(tempLista)
+                    # print(tempLista)
+                    print(ptemp)
+                    # print(str(cc2[2:][:-5]))
+                elif len(tempLista) == 0:
+                    ptemp = 0
+                if cc2[2:][:-5] == "a":
+                    sal = 1
+                    break
+            if sal == 1:
+                break
+
+        self.ids['PULSO'].text = str(round(ptemp, 2))
     # def on_pre_enter(self):
     #     funciones para antes q cargue la pantalla
 
